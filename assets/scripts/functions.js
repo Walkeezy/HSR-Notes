74e60e3cc82df3d7cdeef593526ac66d82a88f84
Kevin Walker <mail@kevinwalker.ch>
2018-05-27 18:03:31 +0200

    Save notes to local storage

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -1,19 +1,45 @@
 jQuery(function ($) {
 
+  // On form submit
   $('.edit-note__form').submit(function(event) {
-    const title = $('#title').val();
-    saveNote(title);
     event.preventDefault();
+    const formdataArray = $(this).serializeArray();
+    const formdataObj = {};
+    $(formdataArray).each(function(i, field){
+      formdataObj[field.name] = field.value;
+    });
+    saveNote(formdataObj);
   });
 
-  function saveNote(title, description, importance, date) {
-    const note = {};
-    note[title] = title;
-    note[description] = description;
-    note[importance] = importance;
-    note[date] = date;
-    console.log(note);
-    localStorage.setItem('notes', note);
-  }
+  // Save new note or update existing one
+  function saveNote(formdata) {
+    let notes = [];
+    const noteid = createID();
+    const note = {
+      id = noteid,
+      title = formdata['title'],
+      description = formdata['description'],
+      importance = formdata['importance'],
+      duedate = formdata['duedate'],
+      date = moment().toISOString(true),
+      date_updated = moment().toISOString(true)
+    };
+    notes.push(note);
+
+    const savedNotes = JSON.parse(localStorage.getItem('notes'));
+
+    if(savedNotes){
+      savedNotes.push(note);
+      localStorage.setItem('notes', JSON.stringify(savedNotes));
+    } else {
+      localStorage.setItem('notes', JSON.stringify(notes));
+    };
+
+  };
+
+  // Create random ID
+  function createID() {
+    return Math.random().toString(36).substr(2, 9);
+  };
 
 });
