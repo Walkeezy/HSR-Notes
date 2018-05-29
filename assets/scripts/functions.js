9a939a5cb895c1b409c7b3ab89a3407d03911045
Kevin Walker <mail@kevinwalker.ch>
2018-05-29 22:38:16 +0200

    Render notes with handlebars

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -1,16 +1,25 @@
 jQuery(function($) {
 
+  // LOAD NOTES
+  //---------------------------------------------------------
+  function renderNotes() {
+    const notes = JSON.parse(localStorage.getItem('notes'));
+    const noteTemplate = $('#noteTemplate').html();
+    const renderedNotes = Handlebars.compile(noteTemplate);
+    $('.notes__list').html(renderedNotes(notes));
+  };
+
+  renderNotes();
+
   // EVENT LISTENERS
   //----------------------------------------------------------
 
   $('.open-edit').on('click', function(event) {
-    $('.edit-note').addClass('open');
-    $('.dimmer').addClass('active');
+    openEdit();
   });
 
   $('.cancel-note').on('click', function(event) {
-    $('.edit-note').removeClass('open');
-    $('.dimmer').removeClass('active');
+    closeEdit();
   });
 
   $('.edit-note__form').submit(function(event) {
@@ -21,32 +30,34 @@ jQuery(function($) {
       formdataObj[field.name] = field.value;
     });
     saveNote(formdataObj);
+    renderNotes();
+    closeEdit();
+    this.reset();
   });
 
   // SAVE / UPDATE NOTE
   //----------------------------------------------------------
   function saveNote(formdata) {
-    if(formdata['note-id'] == ''){
+    if(formdata['note_id'] == ''){
       noteid = createID();
     } else {
-      noteid = formdata['note-id'];
+      noteid = formdata['note_id'];
     };
-    let notes = [];
+    let notes = { notes: [] };
     const note = {
       id = noteid,
       title = formdata['title'],
       description = formdata['description'],
       importance = formdata['importance'],
-      duedate = formdata['due-date'],
+      date_due = formdata['date_due'],
       date = moment().toISOString(true),
-      date_updated = moment().toISOString(true)
     };
-    notes.push(note);
+    notes['notes'].push(note);
 
     const savedNotes = JSON.parse(localStorage.getItem('notes'));
 
     if(savedNotes){
-      savedNotes.push(note);
+      savedNotes['notes'].push(note);
       localStorage.setItem('notes', JSON.stringify(savedNotes));
     } else {
       localStorage.setItem('notes', JSON.stringify(notes));
@@ -58,6 +69,16 @@ jQuery(function($) {
   //----------------------------------------------------------
   function createID() {
     return Math.random().toString(36).substr(2, 6);
+  };
+
+  function openEdit() {
+    $('.edit-note').addClass('open');
+    $('.dimmer').addClass('active');
+  };
+
+  function closeEdit() {
+    $('.edit-note').removeClass('open');
+    $('.dimmer').removeClass('active');
   };
 
 });
