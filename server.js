eb72834ad873b1af2e2c3c88c84fcc537da5fbb0
Kevin Walker <mail@kevinwalker.ch>
2018-06-26 23:15:34 +0200

    Use heroku hostname

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use(bodyParser.json());
 app.use(require('./routes/notes-routes.js'));
 app.use(express.static(__dirname + '/public'));
 
-const hostname = 'localhost';
+const hostname = '0.0.0.0';
 const port = process.env.PORT || 5000;
-app.listen(port, hostname, () => {  console.log(`Server running at http://localhost:${port}`); });
+app.listen(port, hostname, () => {  console.log(`Server running at http://${hostname}:${port}`); });
